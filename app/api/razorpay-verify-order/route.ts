cc9c4e81a098dd5c652733b4f859f1ae6fdbd5be
Adnan <adnanali23189@gmail.com>
2025-08-20 08:43:03 +0530

    Add edge runtime to api routes

--- app/api/razorpay-verify-order/route.ts
+++ app/api/razorpay-verify-order/route.ts
@@ -16,6 +16,8 @@ const generatedSignature = (
   return sig;
 };
 
+export const runtime = 'edge';
+
 export async function POST(request: NextRequest) {
   const { orderId, razorpayPaymentId, razorpaySignature } =
     await request.json();
