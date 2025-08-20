f90abc1c3f6162554659f08f1757fa3ad1166af9
Adnan <adnanali23189@gmail.com>
2025-08-20 10:01:34 +0530

    update cache route get all events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -103,8 +103,8 @@ const ScheduledEventsPage = () => {
   const fetchEventsData = async () => {
     try {
       setLoading(true);
-      // const response = await fetch('https://backend-server.developer-frigus-fiesta.workers.dev/general/get-all-events');
-      const response = await fetch('https://backend-server.developer-frigus-fiesta.workers.dev/general/get-all-events-from-cache');
+      const response = await fetch('https://backend-server.developer-frigus-fiesta.workers.dev/general/get-all-events');
+      // const response = await fetch('https://backend-server.developer-frigus-fiesta.workers.dev/general/get-all-events-from-cache');
       if (!response.ok) {
         throw new Error('Failed to fetch events');
       }
